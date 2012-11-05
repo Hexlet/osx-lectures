7d84420c89104d63b4e1c851c94ed648ba40ba54
Rakhim Davletkaliyev <freetonik@gmail.com>
2012-11-06 00:08:39 +0300

    Lecture 3 code fixed

---
+++ RandomGenerator/AwesomeRandom/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  AwesomeRandom
+//
+//  Created by Rakhim Davletkaliyev on 12-11-05.
+//  Copyright (c) 2012 Rakhim Davletkaliyev. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
